afc3277d9ce97dbdcf7ca9e2d460479f8f746ef7
ADEB <arthurdebruille@proton.me>
2026-08-22 22:19:06 +0200

    cms: add icon for profil tab

--- src/sanity/deskStructure.ts
+++ src/sanity/deskStructure.ts
@@ -1,3 +1,4 @@
+import UserIcon from "@sanity/icons/User";
 import { type StructureResolver } from "sanity/structure";
 
 export const structure: StructureResolver = (S) =>
@@ -6,6 +7,7 @@ export const structure: StructureResolver = (S) =>
     .items([
       S.listItem()
         .title("Profil")
+        .icon(UserIcon)
         .id("profile")
         .child(S.document().schemaType("profile").documentId("profile")),
       S.divider(),
