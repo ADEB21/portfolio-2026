2c010450657c03484a445065e6cc6845a7eb922f
ADEB <arthurdebruille@proton.me>
2026-08-22 22:18:53 +0200

    cms: add theme

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,14 +2,19 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { schemaType } from "./src/sanity";
 import { structure } from "./src/sanity/deskStructure";
+import Favicon from "@components/atoms/Icons/Favicon";
+import {themerTool} from '@sanity/themer/tool'
+import { theme } from "./src/sanity/themes";
 
 export default defineConfig({
   name: "portfolio-2026",
   title: "Portfolio",
+  icon: Favicon,
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  plugins: [structureTool({ structure })],
+  plugins: [structureTool({ structure }), themerTool()],
   schema: {
     types: schemaType,
   },
+  theme: theme
 });
---
+++ src/sanity/themes.ts
@@ -0,0 +1,10 @@
+// sanity/theme.ts
+import { buildTheme } from '@sanity/themer';
+
+export const theme = buildTheme({
+  accent: '#b85b35',
+  background: {
+    light: '#fbf7ee',
+    dark: '#140c09',
+  },
+});
