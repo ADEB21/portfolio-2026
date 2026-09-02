c8dbff25fd90d54c942b2e6fe2e90a6b11c2df13
ADEB <arthurdebruille@proton.me>
2026-09-02 11:50:57 +0200

    cms: change desk structure layout

--- src/sanity/deskStructure.ts
+++ src/sanity/deskStructure.ts
@@ -1,16 +1,68 @@
-import UserIcon from "@sanity/icons/User";
-import { type StructureResolver } from "sanity/structure";
+// src/sanity/structure.ts
+import type { StructureResolver } from "sanity/structure";
+
+import { HomeIcon } from "@sanity/icons/Home";
+import { CaseIcon } from "@sanity/icons/Case";
+import {DocumentsIcon} from '@sanity/icons/Documents'
+import {TagsIcon} from '@sanity/icons/Tags'
+import { CheckmarkCircleIcon } from "@sanity/icons/CheckmarkCircle";
+import { UserIcon } from "@sanity/icons/User";
+import { SparklesIcon } from "@sanity/icons/Sparkles";
+import { CalendarIcon } from "@sanity/icons/Calendar";
+import { UlistIcon } from "@sanity/icons/Ulist";
 
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title("Contenu")
+    .title("Gestion de contenu")
     .items([
+      // 1. Onglet Dossier "Accueil / À propos"
       S.listItem()
-        .title("Profil")
-        .icon(UserIcon)
-        .id("profile")
-        .child(S.document().schemaType("profile").documentId("profile")),
+        .title("Accueil / À propos")
+        .icon(HomeIcon)
+        .child(
+          S.list()
+            .title("Accueil & À propos")
+            .items([
+              S.listItem()
+                .title("Profil")
+                .icon(UserIcon)
+                .child(
+                  S.document().schemaType("profile").documentId("profile"),
+                ),
+
+              S.documentTypeListItem("experience")
+                .title("Expériences")
+                .icon(CalendarIcon),
+
+              S.documentTypeListItem("degree")
+                .title("Diplômes (Degree)")
+                .icon(CheckmarkCircleIcon),
+
+              S.documentTypeListItem("certificate")
+                .title("Certificats")
+                .icon(CheckmarkCircleIcon),
+
+              S.documentTypeListItem("skillCategory")
+                .title("Catégories de compétences (SkillCategory)")
+                .icon(UlistIcon),
+            ]),
+        ),
+
+      // Premier Séparateur
       S.divider(),
-      // Filtrer le profil de la liste automatique des documents
-      ...S.documentTypeListItems().filter((item) => item.getId() !== "profile"),
+
+      // 2. À la racine : Projets & Blog
+      S.documentTypeListItem("project").title("Projets").icon(CaseIcon),
+
+      S.documentTypeListItem("post") // ou le nom exact de ton schéma blog, ex: "blog"
+        .title("Blog")
+        .icon(DocumentsIcon),
+
+      // Deuxième Séparateur
+      S.divider(),
+
+      // 3. Derniers onglets : Données transverses (Skill & Category)
+      S.documentTypeListItem("skill").title("Skills").icon(SparklesIcon),
+
+      S.documentTypeListItem("category").title("Catégories").icon(TagsIcon),
     ]);
